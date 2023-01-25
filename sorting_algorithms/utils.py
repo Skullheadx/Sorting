6400eef413ca75fa580b5bba2f8bf5a84e4251e0
Skullheadx <704277@pdsb.net>
2023-01-24 22:48:49 -0500

    func for check if increasing order in array

--- sorting_algorithms/utils.py
+++ sorting_algorithms/utils.py
@@ -2,3 +2,10 @@ def swap(array, index1, index2):
     get = array[index1], array[index2]
     array[index2], array[index1] = get
     return array
+
+
+def is_increasing(array):
+    for index in range(len(array)-1):
+        if array[index] > array[index + 1]:
+            return False
+    return True
