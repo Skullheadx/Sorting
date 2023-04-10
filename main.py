e66b66d70a7b396ba3be17efdf4dabc48acd835d
Skullheadx <admonty1@gmail.com>
2023-04-10 19:45:52 -0400

    quick sort frames

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from sorting_algorithms.insertion_sort import insertion_sort_frames
 from sorting_algorithms.selection_sort import selection_sort_frames
 from sorting_algorithms.bogo_sort import bogo_sort_frames
 from sorting_algorithms.bubble_sort import bubble_sort_frames
+from sorting_algorithms.quick_sort import quick_sort_frames
 
 
 def main():
     array = [random.random() for _ in range(50)]
-    frames = bubble_sort_frames(array)
+    frames = quick_sort_frames(array)
     window = Display(frames)
     window.show()
 
